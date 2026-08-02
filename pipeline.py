7b08b9e485a184a1fb23fc30e6540c87d535115a
saad <saad@authentiksolution.com>
2026-08-02 22:40:30 +0500

    run research pipleine function crate and call build search agent

---
+++ pipeline.py
@@ -0,0 +1,20 @@
+from agents import build_reader_agent , build_search_agent , writer_chain , critic_chain
+
+def run_research_pipeline(topic : str) -> dict:
+
+    state = {}
+
+    #search agent working 
+    print("\n"+" ="*50)
+    print("step 1 - search agent is working ...")
+    print("="*50)
+
+    search_agent = build_search_agent()
+    search_result = search_agent.invoke({
+        "messages" : [("user", f"Find recent, reliable and detailed information about: {topic}")]
+    })
+    state["search_results"] = search_result['messages'][-1].content
+
+    print("\n search result ",state['search_results'])
+
+   
